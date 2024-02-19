d2f71e7e9eb99bc733e95607bdba304bf9d3625a
nileshsurya1994 <nileshsuryawanshi65@gmail.com>
2024-02-19 13:05:41 +0000

    change index file

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include('includes/dbconnection.php');
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Railway Pass Management System || Home Page</title>
+<title>Airoplain & Railway Pass Management System || Home Page</title>
 
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
 <!-- Custom Theme files -->
